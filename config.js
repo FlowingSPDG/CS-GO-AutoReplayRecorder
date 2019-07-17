553db5bdab757818182802f3ed01517c9e41137f
Flowing <bitoreieru@gmail.com>
2019-07-18 08:26:48 +0900

    wip

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ module.exports = {
     gsi_auth : "SuperUtageGSIAuth", // GSI Auth token
     gotv_delay : 20 , // Delay between live-server and GOTV match,tv_delay - ライブ試合鯖からのGOTV遅延 tv_delay
     killtime_before: 1.5 , // Time to record clip before kill event(sec) - キル発生何秒前かリプレイを保存するか
-    hlae_server_port: 31337, // mirv_pgl server port - mirv_pglサーバーのポート番号
-    hlae_server_path: "/replay" // mirv_pgl server path - mirv_pglサーバーのパス
+    hlae_server_port_live: 31337, // mirv_pgl server port - mirv_pglサーバーのポート番号
+    hlae_server_path_live: "/replay_live", // mirv_pgl server path for live - mirv_pglサーバーのパス
+    hlae_server_port_gotv: 31338, // mirv_pgl server port - mirv_pglサーバーのポート番号
+    hlae_server_path_gotv: "/replay_gotv", // mirv_pgl server path for gotv - mirv_pglサーバーのパス
 }
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,40 @@
-import * as http from "http";
 import mirvpgl from './mirvpgl';
 
 const config = require("../config")
-const pgl = new mirvpgl(config.hlae_server_port, config.hlae_server_path); 
+const client_live = new mirvpgl(config.hlae_server_port_live, config.hlae_server_path_live); 
+const client_gotv = new mirvpgl(config.hlae_server_port_gotv, config.hlae_server_path_gotv); 
 
+const kill_delay = config.gotv_delay - config.killtime_before;
+/*
 const CSGOGSI = require("node-csgo-gsi");
-let gsi = new CSGOGSI();
-gsi.on("event", function() {
- 
+let gsi = new CSGOGSI({
+    port: 4000
 });
+
+gsi.on("all", function(data:any) {
+	//console.log(data)
+});
+*/
+
+client_live.emitter.on('gameEvent', (data) => {
+	var obj = JSON.parse(data)
+	if (obj.name == "player_death") {
+		setTimeout(() => {
+			var cmd = `spec_player_by_accountid ${obj.keys.attacker.xuid};spec_mode 1`
+			console.log(cmd)
+			client_gotv.sendcommand(cmd)
+			client_live.sendcommand(cmd)
+			record_clip()
+		//},kill_delay)
+		},2)
+	}
+})
+
+client_live.emitter.on('error', (err) => {
+	console.error(err)
+})
+
+function record_clip() {
+	// VMIX?
+	// TODO...
+}
