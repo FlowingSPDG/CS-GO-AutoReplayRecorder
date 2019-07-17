8748d32306cca658466c97e038d67e2ba2ec76e0
Flowing <bitoreieru@gmail.com>
2019-07-18 08:28:51 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    gsi_auth : "SuperUtageGSIAuth", // GSI Auth token
+    gsi_auth : "SuperAuth", // GSI Auth token
     gotv_delay : 20 , // Delay between live-server and GOTV match,tv_delay - ライブ試合鯖からのGOTV遅延 tv_delay
     killtime_before: 1.5 , // Time to record clip before kill event(sec) - キル発生何秒前かリプレイを保存するか
     hlae_server_port_live: 31337, // mirv_pgl server port - mirv_pglサーバーのポート番号
