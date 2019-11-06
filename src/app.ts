9c7f924a13e67c16027a409ba88e2dde4b643b66
Flowing <bitoreieru@gmail.com>
2019-11-07 02:56:20 +0900

    implemented vmix

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,12 @@
 import mirvpgl from './mirvpgl';
+const { Connection } = require('vmix-js-utils')
 
 const config = require("../config")
 const client_live = new mirvpgl(config.hlae_server_port_live, config.hlae_server_path_live); 
 const client_gotv = new mirvpgl(config.hlae_server_port_gotv, config.hlae_server_path_gotv); 
+const vmix = new Connection(config.vmix_ip)
 
-const kill_delay = config.tv_delay - config.replay_rec_start_before_kill;
-/*
-const CSGOGSI = require("node-csgo-gsi");
-let gsi = new CSGOGSI({
-    port: 4000
-});
-
-gsi.on("all", function(data:any) {
-	//console.log(data)
-});
-*/
+const kill_delay = (config.tv_delay *1000) - config.replay_rec_start_before_kill;
 
 client_live.emitter.on('gameEvent', (data) => {
 	var obj = JSON.parse(data)
@@ -23,8 +15,7 @@ client_live.emitter.on('gameEvent', (data) => {
 			var cmd = `spec_player_by_accountid ${obj.keys.attacker.xuid};spec_mode 1`
 			console.log(cmd)
 			client_gotv.sendcommand(cmd)
-			client_live.sendcommand(cmd)
-			record_clip()
+			record_clip(config.replay_rec_end_after_kill)
 		//},kill_delay)
 		},2000) //2sec
 	}
@@ -34,7 +25,18 @@ client_live.emitter.on('error', (err) => {
 	console.error(err)
 })
 
-function record_clip() {
-	// VMIX?
-	// TODO...
+let onSuccess = function (response:any) {
+    console.log('Performed command', response)
+}
+let onError = function (error:any) {
+	console.log('Could not perform command', error)
+}
+   
+
+function record_clip(markout:number) {
+	vmix.send({ Function: 'ReplayLive' }, onSuccess, onError)
+	vmix.send({ Function: 'ReplayMarkIn' }, onSuccess, onError)
+	setTimeout(() => {
+		vmix.send({ Function: 'ReplayMarkOut' }, onSuccess, onError)
+	},markout)
 }
